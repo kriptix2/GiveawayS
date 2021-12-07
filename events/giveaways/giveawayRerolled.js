2f4d017815bab527d0b7fab4f1cdc1ce68768939
Aim <91464959+Aim2339@users.noreply.github.com>
2021-12-07 11:00:10 +0530

    Create giveawayRerolled.js

---
+++ events/giveaways/giveawayRerolled.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js")
+module.exports = {
+  async execute(giveaway, winners) {
+    winners.forEach((member) => {
+      member.send({
+        embeds: [new Discord.MessageEmbed()
+          .setTitle(`:tada: Let's goo! We Have A New Winner`)
+          .setColor("#2F3136")
+          .setDescription(`Hello there ${member.user}\n :gift: I heard that the host rerolled and you have won **[[This Giveaway]](https://discord.com/channels/${giveaway.guildId}/${giveaway.channelId}/${giveaway.messageId})**\n :clap: Good Job On Winning **${giveaway.prize}!**\nDM/Ping the host to claim your prize!!`)
+          .setTimestamp()
+          .setFooter(member.user.username, member.user.displayAvatarURL())
+        ]
+      }).catch(e => {})
+    });
+  }
+}
