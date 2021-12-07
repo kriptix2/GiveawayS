63f73b539cb4c14087f67204a428f3253c53098e
Aim <91464959+Aim2339@users.noreply.github.com>
2021-12-07 11:01:40 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,72 @@
+const keepAlive = require("./server")
+const Discord = require("discord.js");
+const client = new Discord.Client({ intents: 7753 });
+const fs = require("fs");
+const config = require("./config.json");
+client.config = config;
+
+const { GiveawaysManager } = require("discord-giveaways");
+client.giveawaysManager = new GiveawaysManager(client, {
+  storage: "./storage/giveaways.json",
+  default: {
+    botsCanWin: false,
+    embedColor: "#2F3136",
+    reaction: "🎉",
+    lastChance: {
+      enabled: true,
+      content: `🛑 **Last chance to enter** 🛑`,
+      threshold: 5000,
+      embedColor: '#FF0000'
+    }
+  }
+});
+
+
+fs.readdir("./events/discord", (_err, files) => {
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    const event = require(`./events/discord/${file}`);
+    let eventName = file.split(".")[0];
+    console.log(`[Event]   ✅  Loaded: ${eventName}`);
+    client.on(eventName, event.bind(null, client));
+    delete require.cache[require.resolve(`./events/discord/${file}`)];
+  });
+});
+
+
+fs.readdir("./events/giveaways", (_err, files) => {
+  files.forEach((file) => {
+    if (!file.endsWith(".js")) return;
+    const event = require(`./events/giveaways/${file}`);
+    let eventName = file.split(".")[0];
+    console.log(`[Event]   🎉 Loaded: ${eventName}`);
+    client.giveawaysManager.on(eventName, (...file) => event.execute(...file, client)), delete require.cache[require.resolve(`./events/giveaways/${file}`)];
+  })
+})
+
+client.commands = new Discord.Collection();
+
+client.interactions = new Discord.Collection();
+
+client.register_arr = []
+fs.readdir("./slash/", (_err, files) => {
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./slash/${file}`);
+    let commandName = file.split(".")[0];
+    client.interactions.set(commandName, {
+      name: commandName,
+      ...props
+    });
+    client.register_arr.push(props)
+  });
+});
+
+client.on("ready", () => {
+  client.user.setPresence({
+    status: "online",
+  });
+});
+
+keepAlive()
+client.login(process.env.TOKEN);
